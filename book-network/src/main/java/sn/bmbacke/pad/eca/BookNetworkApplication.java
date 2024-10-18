edae7b2c7a43c8108f53d73c2552c4a5373575b1
Mbacke Mbaye <mbackembaye74@gmail.com>
2024-10-18 11:34:39 +0000

    init project and add docker file🎉

---
+++ book-network/src/main/java/sn/bmbacke/pad/eca/BookNetworkApplication.java
@@ -0,0 +1,13 @@
+package sn.bmbacke.pad.eca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApplication.class, args);
+	}
+
+}
